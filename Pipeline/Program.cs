be6fa0fc23e5a292577da4ca989c0bbb70ae5086
lab_services_student <lab_services_student@labVMH8OX>
2024-05-28 10:29:19 +0200

    Add project files.

---
+++ Pipeline/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+
+namespace Pipeline
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number one : ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Number two : ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(addition(num1, num2));
+        }
+        static string addition(int num1, int num2)
+        {
+            int answer = num1 + num2;
+            return "The answer is " + num1 + " + " + num2 + " = " + answer;
+        }
+    }
+}
